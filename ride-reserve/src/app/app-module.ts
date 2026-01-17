ce630d8af4e8f78faccf2aabe30d8d1ef3218369
virat122 <vikrantachaudhary96@gmail.com>
2026-01-17 17:21:31 +0530

    1.0.4 | added header navbar

--- ride-reserve/src/app/app-module.ts
+++ ride-reserve/src/app/app-module.ts
@@ -1,16 +1,26 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { LandingModule } from './features/public/landing/landing-module';
+import { Header } from './features/public/header/header';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Header
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LandingModule,
+    BrowserAnimationsModule, 
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
