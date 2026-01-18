c6b46fc49ac9c72033bb4e98447e307253e1cadf
virat122 <vikrantachaudhary96@gmail.com>
2026-01-18 17:14:23 +0530

    1.0.5 | Header nav added

--- ride-reserve/src/app/app-module.ts
+++ ride-reserve/src/app/app-module.ts
@@ -2,7 +2,6 @@ import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { LandingModule } from './features/public/landing/landing-module';
--- ride-reserve/src/app/app-routing-module.ts
+++ ride-reserve/src/app/app-routing-module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Landing } from './features/public/landing/landing/landing';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  
+  { path: 'home',
+    component: Landing
+  },
+  { path: 'login',
+    component: Landing
+  },
+  { path: 'register',
+    component: Landing
+  },
+  {
+    path: '**', redirectTo: '/home'
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
